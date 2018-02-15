a8918cc9c9c1804992fe12fbdc3635aaf3c2473c
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2018-02-16 00:05:40 +0900

    fix

--- exercise-fibonacci-closure.go
+++ exercise-fibonacci-closure.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func fibonacci() func() int {
-
+	f1, f2 := 0, 1
+	return func() int {
+		f := f1
+		f1, f2 = f2, f1+f2
+		return f
+	}
 }
 
 func main() {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+		fmt.Println(i, f())
 	}
 }
